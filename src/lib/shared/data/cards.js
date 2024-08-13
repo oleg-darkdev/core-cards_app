90a4a921b2929e89d96aa797703347b8a2d38be7
DarkDev <darkdev_brand@riseup.net>
2024-08-13 03:46:34 +0300

    intial cards list

---
+++ src/lib/shared/data/cards.js
@@ -0,0 +1,194 @@
+// yellow
+
+const cards = [
+  {
+    title: 'Leaving the meeting, looging entry.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/leavingMeeting.png',
+    id: 1,
+    category: 'yellow'
+  },
+  {
+    title: 'Please mute yourselves.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/pleaseMute.png',
+    id: 2,
+    category: 'yellow'
+  },
+  {
+    title: 'Please hold for just a sec.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/pleaseHold.png',
+    id: 3,
+    category: 'yellow'
+  },
+  {
+    title: 'Try to be boot.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/tryToBeBoot.png',
+    id: 4,
+    category: 'yellow'
+  },
+  {
+    title: 'Slow Down',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/slowDown.png',
+    id: 5,
+    category: 'yellow'
+  },
+  {
+    title: 'I have got to go.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/gotToGo.png',
+    id: 6,
+    category: 'yellow'
+  },
+  {
+    title: 'Be right back.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/brb.png',
+    id: 7,
+    category: 'yellow'
+  },
+
+  {
+    title: 'You`re breaking up.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/breakingUp.png',
+    id: 10,
+    category: 'blue'
+  },
+  {
+    title: 'Can`t hear You.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/cantHearYou.png',
+    id: 11,
+    category: 'blue'
+  },
+  {
+    title: 'Your connection isn`t great.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/yourConnectionBad.png',
+    id: 12,
+    category: 'blue'
+  },
+  {
+    title: 'Actual size.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/actualSize.png',
+    id: 13,
+    category: 'blue'
+  },
+  {
+    title: '404 Error.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/404.png',
+    id: 14,
+    category: 'blue'
+  },
+  {
+    title: 'Someone has a lot of background noise.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/backgroundNoise.png',
+    id: 15,
+    category: 'blue'
+  },
+  {
+    title: 'You`re on mute.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/yourOnMute.png',
+    id: 16,
+    category: 'blue'
+  },
+  {
+    title: 'You`re frozzzzzen.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/frozen.png',
+    id: 17,
+    category: 'blue'
+  },
+
+
+
+
+  {
+    title: 'Enough! Let`s move on.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/elmo.png',
+    id: 20,
+    category: 'green'
+  },
+  {
+    title: 'I like this.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/like.png',
+    id: 21,
+    category: 'green'
+  },
+  {
+    title: 'Awesome!',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/awesome.png',
+    id: 22,
+    category: 'green'
+  },
+  {
+    title: 'Ok, i agree.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/ok.png',
+    id: 23,
+    category: 'green'
+  },
+  {
+    title: 'Green light.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/greenLight.png',
+    id: 24,
+    category: 'green'
+  },
+
+
+  // red
+  {
+    title: '1 minute to end.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/1_minute.png',
+    id: 30,
+    category: 'orange'
+  },
+  {
+    title: '5 minutes to end.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/5_minutes.png',
+    id: 31,
+    category: 'orange'
+  },
+  {
+    title: 'Speed up, please.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/speedUp.png',
+    id: 32,
+    category: 'orange'
+  },
+
+
+
+  {
+    title: 'Unlike.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/unlike.png',
+    id: 40,
+    category: 'red'
+  },
+  {
+    title: 'Red flag.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/redFlag.png',
+    id: 41,
+    category: 'red'
+  },
+
+]
+
+
+export default cards;
