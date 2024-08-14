08bbf23f404f938e47dcd4ee2e89b68b926ebea8
DarkDev <darkdev_brand@riseup.net>
2024-08-14 22:21:45 +0300

    intial data

---
+++ src/lib/shared/data/cardCategories.js
@@ -0,0 +1,54 @@
+const cards = [
+
+  {
+    title: 'Be right back.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/brb.png',
+    id: 7,
+    category: 'yellow',
+    brandColor: 'bg-yellow',
+    textColor: 'text-yellow',
+  },
+
+  {
+    title: 'Actual size.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/actualSize.png',
+    id: 13,
+    category: 'blue',
+    brandColor: 'bg-blue',
+    textColor: 'text-blue',
+  },
+
+  {
+    title: 'Awesome!',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/awesome.png',
+    id: 22,
+    category: 'green',
+    brandColor: 'bg-green',
+    textColor: 'text-green',
+  },
+  {
+    title: 'Speed up, please.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/speedUp.png',
+    id: 32,
+    category: 'orange',
+    brandColor: 'bg-orange',
+    textColor: 'text-orange',
+  },
+  {
+    title: 'Unlike.',
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    img: '/images/cards/unlike.png',
+    id: 40,
+    category: 'red',
+    brandColor: 'bg-red',
+    textColor: 'text-red',
+  },
+]
+
+
+export default cards;
+
