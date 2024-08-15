5c504faf07e32fc6498e6641968cc45c667f67ae
DarkDev <darkdev_brand@riseup.net>
2024-08-15 05:01:37 +0300

    add hamburger menu, adaptate for mobile

--- src/lib/widgets/ui/landing/Header.svelte
+++ src/lib/widgets/ui/landing/Header.svelte
@@ -1,54 +1,31 @@
 <script>
   import {navigation} from '$shared';
+
+  let showMenu = false;
 </script>
 
 <header class="header">
   <nav class="nav_component">
     <div class="container-large">
-      <div class="flex-wrapper flex-align-center flex-justify-space-between">
+      <div class="flex-wrapper flex-align-center justify-between">
         <a href="/" aria-current="page" class="brand w-nav-brand w--current">
           <img src="/images/logo.svg" loading="lazy" class='h-14' alt="logo COREcards">
         </a>
 
 
-        <div class="nav-middle">
-          <div role="banner" class="navbar-2 w-nav">
-            <div class="menu-button-3 w-nav-button">
-              <div class="icon-3 w-icon-nav-menu">
-              </div>
-              <div class="w-embed">
-                <svg width="32" height="21" viewBox="0 0 32 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect width="32" height="3" fill="#ffffff">
-                  </rect>
-                  <rect y="9" width="32" height="3" fill="#ffffff">
-                  </rect>
-                  <rect y="18" width="32" height="3" fill="#ffffff">
-                  </rect>
-                </svg>
-              </div>
-              <div data-is-ix2-target="1" class="lottie-animation-2" data-w-id="f2db3de4-48ab-b1b2-7217-058701fecf9b" data-animation-type="lottie" data-src="https://assets.website-files.com/61fe826977a128ce5854ab54/61fe826977a1285d0054ab76_hamburger-icon.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="2.4791666666666665" data-duration="0">
-              </div>
-            </div>
-            
-            <nav role="navigation" class="nav-menu w-nav-menu">
-              <div class="menu-btn-close w-nav-button">
-                <div class="icon-3 w-icon-nav-menu">
-                </div>
-                <div class="w-embed">
-                  <svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5607 10.4393L2.12134 0L1.75834e-05 2.12132L10.4393 12.5607L0 23L2.12132 25.1213L12.5607 14.682L22.6274 24.7487L24.7488 22.6274L14.682 12.5607L24.7487 2.4939L22.6274 0.372583L12.5607 10.4393Z" fill="#ffffff">
-                    </path>
-                  </svg>
-                </div>
-                <div data-is-ix2-target="1" class="lottie-animation-2" data-w-id="f2db3de4-48ab-b1b2-7217-058701fecfa0" data-animation-type="lottie" data-src="https://assets.website-files.com/61fe826977a128ce5854ab54/61fe826977a1285d0054ab76_hamburger-icon.json" data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="2.4791666666666665" data-duration="0">
-                </div>
-              </div>
-              {#each navigation as {title, link}}
-              <a href="{link}" class="nav-link w-nav-link">
-                {title}
-              </a>
-              {/each}
+        <div class="nav-middle ">
+          <div role="banner" class="navbar-2 w-nav ">
+            <nav role="navigation" class="{showMenu ? 'nav-menu': ''} nav-panel" on:click={()=> showMenu = !showMenu } >
 
+              <!-- <div class="nav-panel {showMenu ? '': ''}"> -->
+                <!-- {#if showMenu} -->
+              {#each navigation as {title, link}}
+                <a href="{link}" class="nav-link w-nav-link">
+                  {title}
+                </a>
+              {/each}
+              <!-- {/if} -->
+              <!-- </div> -->
             </nav>
           </div>
         </div>
@@ -66,6 +43,9 @@
             <img src="/images/6221a310a06c83054e5597e8_new-medium-icon.svg" loading="lazy" width="24" alt="">
           </a> -->
         </div>
+        <div class="h-14 w-14 pt-4 menu-btn" on:click={()=> showMenu = !showMenu }>
+          <img src="/images/menu-icon.svg" class='h-full w-full' alt="menu">
+        </div>
       </div>
     </div>
   </nav>
@@ -73,6 +53,21 @@
 
 
 <style>
+.nav-middle {
+  align-items: center;
+  display: flex;
+}
+.navbar-2 {
+  background-color: rgba(0, 0, 0, 0);
+}
+.navbar-2 {
+  background-color: rgba(0, 0, 0, 0);
+  order: -1;
+  position: static;
+}
+
+
+
 .header {
   z-index: 50;
   width: 100%;
@@ -81,11 +76,64 @@
   padding-bottom: 15px;
   position: fixed;
 }
+@media screen and (min-width: 991px) {
+  .menu-btn {
+    display: none;
+  }
+
+}
 
 @media screen and (max-width: 991px) {
+  .menu-button-3 {
+    z-index: 300;
+    padding-left: 0;
+    padding-right: 18px;
+  }
+
+  .nav-panel {
+    display: none;
+  }
+  .menu-button-3 {
+    z-index: 0;
+    background-color: rgba(0, 0, 0, 0);
+    position: relative;
+    left: 0;
+    right: 0;
+  }
+
+
+
+
+  .nav-menu {
+  width: 100%;
+  height: 100vh;
+  max-width: none;
+  text-align: center;
+  background-color: #0e0e13;
+  flex-flow: column;
+  justify-content: space-between;
+  align-items: stretch;
+  padding-top: 25%;
+  display: flex;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index:10;
+}
+
+
   .nav_component {
     padding-left: 12px;
     padding-right: 12px;
+  }
+  .nav-middle {
+    flex: 0 auto;
+    justify-content: space-between;
+  }
+  .header {
+    z-index: 10;
   }
 }
 </style>
