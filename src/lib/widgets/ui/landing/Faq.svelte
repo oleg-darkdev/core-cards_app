63173bf1c624d8b51da29c80e9d4f5152e37ab9a
DarkDev <darkdev_brand@riseup.net>
2024-08-17 00:27:36 +0300

    refactoring import of data

--- src/lib/widgets/ui/landing/Faq.svelte
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -1,48 +1,5 @@
 <script>
-  const faq = [
-    {
-      q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      a: [
-        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-        'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      ]
-    },
-    {
-      q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      a: [
-        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-        'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      ]
-    },
-    {
-      q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      a: [
-        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-        'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      ]
-    },
-    {
-      q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      a: [
-        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-        'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      ]
-    },
-    {
-      q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      a: [
-        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-        'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      ]
-    },
-    {
-      q: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-      a: [
-        `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.`,
-        'When an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      ]
-    },
-  ]
+  import {faq} from '$shared';
 </script>
 
 
