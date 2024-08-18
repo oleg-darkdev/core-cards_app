03a75619ed0df5edeaeca179a8a68e750c31be37
DarkDev <darkdev_brand@riseup.net>
2024-08-19 01:34:21 +0300

    fix layout of cards for mobile, tablet, desktop

--- src/lib/widgets/ui/landing/GalleryWithoutShadow.svelte
+++ src/lib/widgets/ui/landing/GalleryWithoutShadow.svelte
@@ -3,12 +3,12 @@
 </script>
 
 
-<section class="h-[80vh] overflow-hidden mb-20" >
-<div class="flex flex-row overflow-x-hidden h-full justify-center items-center">
+<section class="h-auto overflow-hidden " >
+<div class="flex lg:flex-row lg:flex-wrap overflow-hidden h-full justify-center items-center py-14 ">
   {#each cards.cards as {img, title, desc}, id}
-    <a href="" class='{id % 2 === 0 ? 'translate-y-12': '-translate-y-12'}'>
-      <div class=" bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8 transition duration-300 ease-in-out delay-150 transform  ">
-          <img class="object-cover object-center w-full  rounded-xl" src="{img}" alt="{title} card image">
+    <a href="" class='{id % 2 === 0 ? 'translate-y-12': '-translate-y-12'} my-1 mx-1'>
+      <div class=" bg-white shadow-2xl rounded-xl lg:h-80 lg:w-64 md:w-40 md:h-64 h-64 w-40  md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8 transition duration-300 ease-in-out delay-150 transform  ">
+          <img class="object-cover object-center w-full h-full rounded-xl" src="{img}" alt="{title} card image">
       </div>
     </a>
   {/each}
